3b6e3fe1255503b34289e2f70441c570f8599ee1
Xiangjian Meng <mengxiangjian13@163.com>
2014-12-14 22:59:03 +0800

    add city viewcontroller

---
+++ GoodDay/GoodDay/CityViewController.h
@@ -0,0 +1,13 @@
+//
+//  CityViewController.h
+//  GoodDay
+//
+//  Created by Xiangjian Meng on 14/12/14.
+//  Copyright (c) 2014年 cn.com.modernmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CityViewController : UIViewController
+
+@end
---
+++ GoodDay/GoodDay/CityViewController.m
@@ -0,0 +1,21 @@
+//
+//  CityViewController.m
+//  GoodDay
+//
+//  Created by Xiangjian Meng on 14/12/14.
+//  Copyright (c) 2014年 cn.com.modernmedia. All rights reserved.
+//
+
+#import "CityViewController.h"
+
+@implementation CityViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    
+}
+
+@end
